1188ffc9a32052b5f3778b143c40c52a8dc6ed4f
Pirabuon <40371722+Pirabuon@users.noreply.github.com>
2023-03-25 13:04:46 +0100

    Update [slug].js

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -4,24 +4,20 @@ import styles from "../../styles/post.module.css";
 
 export default function Post(props) {
   const router = useRouter();
-
-  if (router.isFallback) {
-    return <div>Loading...</div>;
-  }
-
   return (
     <>
       <p>
         <Link href="/blog">
-          <small>&laquo; back to all blog posts</small>
+          <small>&laquo; back</small>
         </Link>
       </p>
       <h2 className={styles.title}>{props.post.title.rendered}</h2>
       <div
+        className={styles.cont}
         dangerouslySetInnerHTML={{ __html: props.post.content.rendered }}
       ></div>
       <button className={styles.button} onClick={() => router.push("/blog")}>
-        Click me to programmatically navigate or redirect
+        Back
       </button>
     </>
   );
@@ -30,16 +26,20 @@ export default function Post(props) {
 export async function getStaticPaths() {
   const res = await fetch("https://valaakam.com/wp-json/wp/v2/posts");
   const posts = await res.json();
-  const paths = posts.slice(0, 10).map((post) => ({
-    params: { slug: post.slug },
-  }));
+  const thePaths = posts.map((post) => {
+    return { params: { slug: post.slug } };
+  });
 
-  return { paths, fallback: 'blocking' };
+  return {
+    paths: thePaths,
+    fallback: false,
+  };
 }
 
-export async function getStaticProps({ params }) {
+export async function getStaticProps(context) {
+  const slug = context.params.slug;
   const res = await fetch(
-    `https://valaakam.com/wp-json/wp/v2/posts?slug=${params.slug}&_fields=title,content`
+    `https://valaakam.com/wp-json/wp/v2/posts?slug=${slug}&_fields=title,content`
   );
   const post = await res.json();
 
@@ -47,6 +47,6 @@ export async function getStaticProps({ params }) {
     props: {
       post: post[0],
     },
-    revalidate: 1,
+    revalidate: 10,
   };
 }
