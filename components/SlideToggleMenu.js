a5a5271c303e69ded12bad517bf23da82c17349b
Pirabuon <40371722+Pirabuon@users.noreply.github.com>
2023-03-26 01:14:04 +0100

    Create SlideToggleMenu.js

---
+++ components/SlideToggleMenu.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+import styles from "../styles/SlideToggleMenu.module.css";
+
+function SlideToggleMenu() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function toggleMenu() {
+    setIsOpen(!isOpen);
+  }
+
+  return (
+    <div className={styles.slideToggleMenu}>
+      <button className={styles.toggleButton} onClick={toggleMenu}>
+        {isOpen ? "Close Menu" : "Open Menu"}
+      </button>
+      <nav className={`${styles.menu} ${isOpen ? styles.isOpen : ""}`}>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">abnormal</a>
+          </li>
+          <li>
+            <a href="/">blog</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default SlideToggleMenu;
