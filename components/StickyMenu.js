c72c3d77aa3ccb00d5c95d314b247aa1ce7dcbbf
Pirabuon <40371722+Pirabuon@users.noreply.github.com>
2023-03-29 23:29:29 +0200

    Add sticky menu

---
+++ components/StickyMenu.js
@@ -0,0 +1,55 @@
+import styles from "../styles/SlideToggleMenu.module.css";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+function StickyMenu() {
+  const router = useRouter();
+
+  return (
+    <div className={styles.stickMenuHolder}>
+      <div className="stickMenu">
+        <ul>
+          <li>
+            <Link
+              className={
+                router.pathname == "/blog" || router.pathname == "/blog/[slug]"
+                  ? "active"
+                  : ""
+              }
+              href="/"
+            >
+              <img
+                src="https://www.meilleurservice.com/images/cat_22-03-2023_43cat_21-09-2022_43friend.webp"
+                style={{ width: "20px", height: "auto" }}
+              />
+              Name
+            </Link>
+          </li>
+          <li>
+            <img
+              src="https://www.meilleurservice.com/images/cat_22-03-2023_43cat_21-09-2022_43friend.webp"
+              style={{ width: "20px", height: "auto" }}
+            />
+            Name
+          </li>
+          <li>
+            <img
+              src="https://www.meilleurservice.com/images/cat_22-03-2023_43cat_21-09-2022_43friend.webp"
+              style={{ width: "20px", height: "auto" }}
+            />
+            Name
+          </li>
+          <li>
+            <img
+              src="https://www.meilleurservice.com/images/cat_22-03-2023_43cat_21-09-2022_43friend.webp"
+              style={{ width: "20px", height: "auto" }}
+            />
+            Name
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default StickyMenu;
