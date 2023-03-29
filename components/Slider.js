a7767ee3a6e685cf313829651d97e82d47446c2d
Pirabuon <40371722+Pirabuon@users.noreply.github.com>
2023-03-30 00:01:08 +0200

    Update Slider.js

--- components/Slider.js
+++ components/Slider.js
@@ -1,6 +1,3 @@
-import React, { useEffect, useState } from "react";
-import Image from "next/image";
-
 import SwiperCore, { Autoplay } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper-bundle.css";
@@ -9,47 +6,45 @@ SwiperCore.use([Autoplay]);
 
 export default function App() {
   return (
-    <>
-      <Swiper
-          slidesPerView={3}
-          speed={1000}
-          loop={true}
-          autoplay={{ delay: 3000 }}
-          breakpoints={{
-            640: {
-              slidesPerView: 3,
-              spaceBetween: 20,
-            },
-            768: {
-              slidesPerView: 4,
-              spaceBetween: 40,
-            },
-            1024: {
-              slidesPerView: 5,
-              spaceBetween: 50,
-            },
-          }}
-        className="mySwiper specialPromo"
-      >
-        <SwiperSlide>
-          <div className="promoItem">Hello</div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="promoItem">World</div>
-        </SwiperSlide>
-  <SwiperSlide>
-          <div className="promoItem">Hello</div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="promoItem">World</div>
-        </SwiperSlide>
-  <SwiperSlide>
-          <div className="promoItem">Hello</div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="promoItem">World</div>
-        </SwiperSlide>
-      </Swiper>
-    </>
+    <Swiper
+      slidesPerView={3}
+      speed={1000}
+      loop={true}
+      autoplay={{ delay: 3000 }}
+      breakpoints={{
+        640: {
+          slidesPerView: 3,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 5,
+          spaceBetween: 50,
+        },
+      }}
+      className="mySwiper specialPromo"
+    >
+      <SwiperSlide key={1}>
+        <div className="promoItem">Hello</div>
+      </SwiperSlide>
+      <SwiperSlide key={2}>
+        <div className="promoItem">World</div>
+      </SwiperSlide>
+      <SwiperSlide key={3}>
+        <div className="promoItem">Post</div>
+      </SwiperSlide>
+      <SwiperSlide key={4}>
+        <div className="promoItem">grid</div>
+      </SwiperSlide>
+      <SwiperSlide key={5}>
+        <div className="promoItem">cat</div>
+      </SwiperSlide>
+      <SwiperSlide key={6}>
+        <div className="promoItem">list</div>
+      </SwiperSlide>
+    </Swiper>
   );
 }
