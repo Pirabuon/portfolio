643090f29f14fd0beef16fa1f9e51aa6cd1d8a80
Pirabuon <40371722+Pirabuon@users.noreply.github.com>
2023-04-02 20:50:22 +0200

    Cat list add index

--- pages/index.js
+++ pages/index.js
@@ -1,57 +1,109 @@
 import Link from "next/link";
-import catScience from "../components/catScience.js";
 
 export default function Blog(props) {
   return (
     <>
-    <catScience/>
-      <h2></h2>
-    <div className="lister">
-      {props.posts.map((post, index) => {
-        let featuredImageUrl =
-          post?._embedded?.["wp:featuredmedia"]?.[0]?.media_details?.sizes
-            ?.medium?.source_url;
-        if (!featuredImageUrl) {
-          // use first image from post if no featured image available
-          const matches = post.content.rendered.match(/<img.*?src="(.*?)"/);
-          if (matches) {
-            featuredImageUrl = matches[1];
+      <h2>Science</h2>
+      <div className="lister">
+        {props.sciencePosts.map((post, index) => {
+          let featuredImageUrl =
+            post?._embedded?.["wp:featuredmedia"]?.[0]?.media_details?.sizes
+              ?.medium?.source_url;
+          if (!featuredImageUrl) {
+            // use first image from post if no featured image available
+            const matches = post.content.rendered.match(/<img.*?src="(.*?)"/);
+            if (matches) {
+              featuredImageUrl = matches[1];
+            }
           }
-        }
-        return (
-          <Link href={`/blog/${post.slug}`}>
-            <div key={index} className="postItem">
-              {featuredImageUrl && (
-                <img
-                  className="mainImg"
-                  src={featuredImageUrl}
-                  alt={post.title.rendered}
-                />
-              )}
-              <div className="postCont">
-                <h3>{post.title.rendered}</h3>
-                <div
-                  className="desc"
-                  dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }}
-                ></div>
+          return (
+            <Link href={`/blog/${post.slug}`}>
+              <div key={index} className="postItem">
+                {featuredImageUrl && (
+                  <img
+                    className="mainImg"
+                    src={featuredImageUrl}
+                    alt={post.title.rendered}
+                  />
+                )}
+                <div className="postCont">
+                  <h3>{post.title.rendered}</h3>
+                  <div
+                    className="desc"
+                    dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }}
+                  ></div>
+                </div>
               </div>
-            </div>
-          </Link>
-        );
-      })}
+            </Link>
+          );
+        })}
+      </div>
+
+      <h2>Bio</h2>
+      <div className="lister">
+        {props.bioPosts.map((post, index) => {
+          let featuredImageUrl =
+            post?._embedded?.["wp:featuredmedia"]?.[0]?.media_details?.sizes
+              ?.medium?.source_url;
+          if (!featuredImageUrl) {
+            // use first image from post if no featured image available
+            const matches = post.content.rendered.match(/<img.*?src="(.*?)"/);
+            if (matches) {
+              featuredImageUrl = matches[1];
+            }
+          }
+          return (
+            <Link href={`/blog/${post.slug}`}>
+              <div key={index} className="postItem">
+                {featuredImageUrl && (
+                  <img
+                    className="mainImg"
+                    src={featuredImageUrl}
+                    alt={post.title.rendered}
+                  />
+                )}
+                <div className="postCont">
+                  <h3>{post.title.rendered}</h3>
+                  <div
+                    className="desc"
+                    dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }}
+                  ></div>
+                </div>
+              </div>
+            </Link>
+          );
+        })}
       </div>
     </>
   );
 }
 
+
 export async function getStaticProps() {
-  const response = await fetch(
-    "https://valaakam.com/wp-json/wp/v2/posts?_embed=true"
+  const scienceResponse = await fetch(
+    "https://valaakam.com/wp-json/wp/v2/posts?_embed=true&categories=7&per_page=5"
   );
-  const data = await response.json();
+  const scienceData = await scienceResponse.json();
+
+  const bioResponse = await fetch(
+    "https://valaakam.com/wp-json/wp/v2/posts?_embed=true&categories=8&per_page=5"
+  );
+  const bioData = await bioResponse.json();
 
   // modify the post data to include author name, category name, and formatted date
-  const modifiedData = data.map((post) => ({
+  const modifiedScienceData = scienceData.map((post) => ({
+    ...post,
+    author_name: post?._embedded?.author?.[0]?.name ?? "Unknown Author",
+    category_name:
+      post?._embedded?.["wp:term"]?.[0]?.[0]?.name ?? "Uncategorized",
+    date: new Date(post.date).toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    }),
+  }));
+
+  const modifiedBioData = bioData.map((post) => ({
     ...post,
     author_name: post?._embedded?.author?.[0]?.name ?? "Unknown Author",
     category_name:
@@ -65,7 +117,8 @@ export async function getStaticProps() {
 
   return {
     props: {
-      posts: modifiedData,
+      sciencePosts: modifiedScienceData,
+      bioPosts: modifiedBioData,
     },
     revalidate: 10, // update content every 10 seconds
   };
