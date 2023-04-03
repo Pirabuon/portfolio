43aee930da366ac78eeb21af1dc61b7b36972e9f
Pirabuon <40371722+Pirabuon@users.noreply.github.com>
2023-04-03 15:04:35 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Link from "next/link";
 
-function renderPosts(posts, category) {
+export default function Blog(props) {
   return (
     <>
-      <h2>{category}</h2>
+    <div className="home">
+      <h2>Science</h2>
       <div className="lister">
-        {posts.map((post, index) => {
+        {props.sciencePosts.map((post, index) => {
           let featuredImageUrl =
             post?._embedded?.["wp:featuredmedia"]?.[0]?.media_details?.sizes
               ?.medium?.source_url;
@@ -17,8 +18,8 @@ function renderPosts(posts, category) {
             }
           }
           return (
-            <Link href={`/blog/${post.slug}`} key={index}>
-              <div className="postItem">
+            <Link href={`/blog/${post.slug}`}>
+              <div key={index} className="postItem">
                 {featuredImageUrl && (
                   <img
                     className="mainImg"
@@ -38,24 +39,47 @@ function renderPosts(posts, category) {
           );
         })}
       </div>
-    </>
-  );
-}
 
-export default function Blog(props) {
-  return (
-    <>
-      <div className="home">
-        {renderPosts(props.sciencePosts, "Science")}
-        {renderPosts(props.bioPosts, "Bio")}
-        {renderPosts(props.abnormal, "Abnormal")}
-        {renderPosts(props.puzzle, "Puzzle")}
-
-        {/* Add more categories of posts here */}
+      <h2>Bio</h2>
+      <div className="lister">
+        {props.bioPosts.map((post, index) => {
+          let featuredImageUrl =
+            post?._embedded?.["wp:featuredmedia"]?.[0]?.media_details?.sizes
+              ?.medium?.source_url;
+          if (!featuredImageUrl) {
+            // use first image from post if no featured image available
+            const matches = post.content.rendered.match(/<img.*?src="(.*?)"/);
+            if (matches) {
+              featuredImageUrl = matches[1];
+            }
+          }
+          return (
+            <Link href={`/blog/${post.slug}`}>
+              <div key={index} className="postItem">
+                {featuredImageUrl && (
+                  <img
+                    className="mainImg"
+                    src={featuredImageUrl}
+                    alt={post.title.rendered}
+                  />
+                )}
+                <div className="postCont">
+                  <h3>{post.title.rendered}</h3>
+                  <div
+                    className="desc"
+                    dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }}
+                  ></div>
+                </div>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
       </div>
     </>
   );
 }
+
 
 export async function getStaticProps() {
   const scienceResponse = await fetch(
@@ -68,21 +92,8 @@ export async function getStaticProps() {
   );
   const bioData = await bioResponse.json();
 
-  const abnormalResponse = await fetch(
-    "https://valaakam.com/wp-json/wp/v2/posts?_embed=true&categories=9&per_page=4"
-  );
-  const abnormalData = await abnormalResponse.json();
-
-
-  const puzzleResponse = await fetch(
-    "https://valaakam.com/wp-json/wp/v2/posts?_embed=true&categories=10&per_page=4"
-  );
-  const puzzleData = await puzzleResponse.json();
-
-
-
   // modify the post data to include author name, category name, and formatted date
-  const modifyPostData = (post) => ({
+  const modifiedScienceData = scienceData.map((post) => ({
     ...post,
     author_name: post?._embedded?.author?.[0]?.name ?? "Unknown Author",
     category_name:
@@ -92,21 +103,24 @@ export async function getStaticProps() {
       month: "long",
       day: "numeric",
     }),
-  });
+  }));
 
-  const modifiedScienceData = scienceData.map(modifyPostData);
-  const modifiedBioData = bioData.map(modifyPostData);
-  const modifiedAbnormalData = abnormalData.map(modifyPostData);
-  const modifiedPuzzleData = puzzleData.map(modifyPostData);
+  const modifiedBioData = bioData.map((post) => ({
+    ...post,
+    author_name: post?._embedded?.author?.[0]?.name ?? "Unknown Author",
+    category_name:
+      post?._embedded?.["wp:term"]?.[0]?.[0]?.name ?? "Uncategorized",
+    date: new Date(post.date).toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    }),
+  }));
 
   return {
     props: {
       sciencePosts: modifiedScienceData,
       bioPosts: modifiedBioData,
-      abnormalPosts: modifiedAbnormalData,
-      puzzlePosts: modifiedPuzzleData,
-
-      // Add more categories of posts here
     },
     revalidate: 10, // update content every 10 seconds
   };
