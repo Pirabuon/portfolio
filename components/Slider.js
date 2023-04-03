266d1761ea49924c73b09bb8631edfae67dbbb13
Pirabuon <40371722+Pirabuon@users.noreply.github.com>
2023-04-03 12:42:44 +0200

    Update Slider.js

--- components/Slider.js
+++ components/Slider.js
@@ -14,31 +14,31 @@ export default function App() {
       id: 1,
       href: "/science",
       label: "அறிவியல்",
-      imgUrl: "https://www.meilleurservice.com/images/cat_22-03-2023_43cat_21-09-2022_43friend.webp",
+      imgUrl: "https://valaakam.com/wp-content/uploads/2023/04/atom.png",
     },
     {
       id: 2,
       href: "/abnormal",
       label: "அமானுடம்",
-      imgUrl: "https://www.meilleurservice.com/images/cat_22-03-2023_43cat_21-09-2022_43friend.webp",
+      imgUrl: "https://valaakam.com/wp-content/uploads/2023/04/ufo.png",
     },
     {
       id: 3,
       href: "/history",
       label: "வரலாறு",
-      imgUrl: "https://www.meilleurservice.com/images/cat_22-03-2023_43cat_21-09-2022_43friend.webp",
+      imgUrl: "https://valaakam.com/wp-content/uploads/2023/04/history.png",
     },
     {
       id: 4,
       href: "/mystery",
       label: "வினோதங்கள்",
-      imgUrl: "https://www.meilleurservice.com/images/cat_22-03-2023_43cat_21-09-2022_43friend.webp",
+      imgUrl: "https://valaakam.com/wp-content/uploads/2023/04/mystery.png",
     },
     {
       id: 5,
       href: "/scholars",
       label: "மேதைகள்",
-      imgUrl: "https://www.meilleurservice.com/images/cat_22-03-2023_43cat_21-09-2022_43friend.webp",
+      imgUrl: "https://valaakam.com/wp-content/uploads/2023/04/einstein.png",
     },
   ]);
 
