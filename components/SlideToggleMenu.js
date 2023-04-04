bb86c432155268ad48df3bf4ba6d1740b1ae611e
Pirabuon <40371722+Pirabuon@users.noreply.github.com>
2023-04-04 23:45:54 +0200

    Update SlideToggleMenu.js

--- components/SlideToggleMenu.js
+++ components/SlideToggleMenu.js
@@ -1,38 +1,62 @@
 import { useState } from "react";
-
+import Link from "next/link"; // Added import for Link component
+import { useRouter } from "next/router"; // Added import for useRouter hook
 import styles from "../styles/SlideToggleMenu.module.css";
 
-function SlideToggleMenu() {
+export default function SlideToggleMenu() { // Removed the duplicate function declaration
+  const router = useRouter();
   const [isOpen, setIsOpen] = useState(false);
+  const [slides, setSlides] = useState([
+    {
+      id: 1,
+      href: "/science",
+      label: "அறிவியல்",
+      imgUrl: "https://valaakam.com/wp-content/uploads/2023/04/atom.png",
+    },
+    {
+      id: 2,
+      href: "/abnormal",
+      label: "அமானுடம்",
+      imgUrl: "https://valaakam.com/wp-content/uploads/2023/04/ufo.png",
+    },
+    {
+      id: 3,
+      href: "/history",
+      label: "வரலாறு",
+      imgUrl: "https://valaakam.com/wp-content/uploads/2023/04/history.png",
+    },
+    {
+      id: 4,
+      href: "/mystery",
+      label: "வினோதங்கள்",
+      imgUrl: "https://valaakam.com/wp-content/uploads/2023/04/mystery.png",
+    },
+    {
+      id: 5,
+      href: "/scholars",
+      label: "மேதைகள்",
+      imgUrl: "https://valaakam.com/wp-content/uploads/2023/04/einstein.png",
+    },
+  ]);
 
   function toggleMenu() {
     setIsOpen(!isOpen);
   }
 
   return (
-    <div className={styles.slideToggleMenu}>
-      <button className={styles.hamburgerButton} onClick={toggleMenu}>
-        <div className={isOpen ? styles.closeIcon : styles.hamburgerIcon}>
-          <span className={styles.iconBar}></span>
-          <span className={styles.iconBar}></span>
-          <span className={styles.iconBar}></span>
-        </div>
-      </button>
-      <nav className={`${styles.menu} ${isOpen ? styles.isOpen : ""}`}>
-        <ul>
-          <li>
-            <a href="/">Home</a>
+    <nav className={`${styles.menu} ${isOpen ? styles.isOpen : ""}`}>
+      <ul>
+        {slides.map((slide) => (
+          <li key={slide.id}>
+            <Link href={slide.href}>
+              <a className={`${router.pathname === slide.href ? "active" : ""}`}>
+                <img src={slide.imgUrl} style={{ width: "20px", height: "auto" }} />
+                {slide.label}
+              </a>
+            </Link>
           </li>
-          <li>
-            <a href="/abnormal">abnormal</a>
-          </li>
-          <li>
-            <a href="/blog">blog</a>
-          </li>
-        </ul>
-      </nav>
-    </div>
+        ))}
+      </ul>
+    </nav>
   );
 }
-
-export default SlideToggleMenu;
