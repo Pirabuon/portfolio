67d228312da1e685977196d42a3648342799e7e5
Pirabuon <40371722+Pirabuon@users.noreply.github.com>
2023-04-05 17:23:09 +0200

    Create GridMenu.js

---
+++ components/GridMenu.js
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import Link from "next/link"; // Added import for Link component
+import { useRouter } from "next/router"; // Added import for useRouter hook
+import styles from "../styles/SlideToggleMenu.module.css";
+
+export default function SlideToggleMenu() {
+  const router = useRouter();
+  const [isOpen, setIsOpen] = useState(false);
+  const [slides, setSlides] = useState([
+    {
+      id: 1,
+      href: "/science",
+      label: "அறிவியல்",
+      imgUrl: "https://valaakam.com/wp-content/uploads/2023/04/atom.png",
+    },
+    {
+      id: 2,
+      href: "/abnormal",
+      label: "அமானுடம்",
+      imgUrl: "https://valaakam.com/wp-content/uploads/2023/04/ufo.png",
+    },
+    {
+      id: 3,
+      href: "/history",
+      label: "வரலாறு",
+      imgUrl: "https://valaakam.com/wp-content/uploads/2023/04/history.png",
+    },
+    {
+      id: 4,
+      href: "/mystery",
+      label: "வினோதங்கள்",
+      imgUrl: "https://valaakam.com/wp-content/uploads/2023/04/mystery.png",
+    },
+    {
+      id: 5,
+      href: "/scholars",
+      label: "மேதைகள்",
+      imgUrl: "https://valaakam.com/wp-content/uploads/2023/04/einstein.png",
+    },
+  ]);
+
+  function toggleMenu() {
+    setIsOpen(!isOpen);
+  }
+  
+  function handleClick(id) {
+    setIsOpen(false);
+  }
+
+  return (
+    <div className={styles.slideToggleMenu}>
+      <button className={styles.hamburgerButton} onClick={toggleMenu}>
+     grid
+      </button>
+      <nav className={`${styles.menu} ${isOpen ? styles.isOpen : ""}`}>
+        <ul>
+          {slides.map((slide) => (
+            <li key={slide.id} onClick={() => handleClick(slide.id)}>
+              <Link href={slide.href}>
+                <a className={`${router.pathname === slide.href ? "active" : ""}`}>
+                  <img src={slide.imgUrl} style={{ width: "20px", height: "auto" }} />
+                  {slide.label}
+                </a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
+  );
+}
