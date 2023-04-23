8c269bddc407b2f739f9e1f348b5f67a954d0616
Pirabuon <40371722+Pirabuon@users.noreply.github.com>
2023-04-23 23:50:43 +0200

    Update Login.js

--- components/Login.js
+++ components/Login.js
@@ -1,33 +1,10 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState(null);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [userData, setUserData] = useState(null);
-
-  useEffect(() => {
-    const fetchUserData = async () => {
-      const token = localStorage.getItem('token');
-      if (!token) {
-        return;
-      }
-      try {
-        const response = await fetch('https://valaakam.com/wp-json/wp/v2/users/me', {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
-        const data = await response.json();
-        setUserData(data);
-      } catch (error) {
-        console.error(error);
-        setError('An error occurred while fetching user data.');
-      }
-    };
-    fetchUserData();
-  }, [isLoggedIn]);
 
   const handleLogin = async (event) => {
     event.preventDefault();
@@ -59,17 +36,12 @@ const Login = () => {
   const handleLogout = () => {
     localStorage.removeItem('token');
     setIsLoggedIn(false);
-    setUserData(null);
   };
 
   return (
     <>
-      {userData ? (
-        <>
-          <p>Welcome, {userData.name} ({userData.email}).</p>
-          <p>You registered on {new Date(userData.registered_date).toLocaleDateString()}.</p>
-          <p>You have {userData.post_count} posts.</p>
-        </>
+      {isLoggedIn ? (
+        <p>Hello!</p>
       ) : (
         <form onSubmit={handleLogin}>
           <div>
