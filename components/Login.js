4f14b8919e7220494057d778706319ee9e75bf02
Pirabuon <40371722+Pirabuon@users.noreply.github.com>
2023-04-24 01:19:44 +0200

    Update Login.js

--- components/Login.js
+++ components/Login.js
@@ -1,6 +1,11 @@
+is following code correct?
+
 import { useState, useEffect } from 'react';
 
 const Login = () => {
+  const [title, setTitle] = useState('');
+const [content, setContent] = useState('');
+
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState(null);
@@ -13,6 +18,35 @@ const Login = () => {
       setIsLoggedIn(true);
     }
   }, []);
+
+  const handleCreatePost = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch('https://valaakam.com/wp-json/wp/v2/posts', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+        body: JSON.stringify({
+          title: title,
+          content: content,
+          status: 'publish',
+        }),
+      });
+      const data = await response.json();
+      if (!response.ok) {
+        setError(data.message);
+        return;
+      }
+      setTitle('');
+      setContent('');
+    } catch (error) {
+      console.error(error);
+      setError('An error occurred while creating the post. Please try again later.');
+    }
+  };
+  
 
   const handleLogin = async (event) => {
     event.preventDefault();
@@ -58,37 +92,61 @@ const Login = () => {
   };
 
   return (
+    
     <>
-      {isLoggedIn ? (
-        <p>Hello {username}, your email id is {email}!</p>
-      ) : (
-        <form onSubmit={handleLogin}>
-          <div>
-            <label htmlFor="username">Username</label>
-            <input
-              type="text"
-              id="username"
-              value={username}
-              onChange={(event) => setUsername(event.target.value)}
-            />
-          </div>
-          <div>
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(event) => setPassword(event.target.value)}
-            />
-          </div>
-          <button type="submit">Log in</button>
-          {error && <p>{error}</p>}
-        </form>
-      )}
-      {isLoggedIn && (
-        <button onClick={handleLogout}>Log out</button>
-      )}
+  {isLoggedIn ? (
+    <>
+      <p>Hello {username}, your email id is {email}!</p>
+      <form onSubmit={handleCreatePost}>
+        <div>
+          <label htmlFor="title">Title</label>
+          <input
+            type="text"
+            id="title"
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="content">Content</label>
+          <textarea
+            id="content"
+            value={content}
+            onChange={(event) => setContent(event.target.value)}
+          />
+        </div>
+        <button type="submit">Create Post</button>
+      </form>
     </>
+  ) : (
+    <form onSubmit={handleLogin}>
+      <div>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+      </div>
+      <button type="submit">Log in</button>
+      {error && <p>{error}</p>}
+    </form>
+  )}
+  {isLoggedIn && (
+    <button onClick={handleLogout}>Log out</button>
+  )}
+</>
+
   );
 };
 
