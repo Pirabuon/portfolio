5df2732e076cc7a7fe8851c1b7f6a3ab9ddc234b
Pirabuon <40371722+Pirabuon@users.noreply.github.com>
2023-08-17 01:58:11 +0200

    Update Login.js

--- components/Login.js
+++ components/Login.js
@@ -1,159 +1,64 @@
 import { useState, useEffect } from 'react';
 
-const API_URL = 'https://pirabu.com/wp-json';
+import { useState } from 'react';
+import Cookies from 'js-cookie';
 
-const Login = () => {
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
+export default function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [error, setError] = useState(null);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [email, setEmail] = useState('');
 
-  useEffect(() => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      setIsLoggedIn(true);
-      fetchUserDetails(token);
-    }
-  }, []);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-  const handleCreatePost = async (event) => {
-    event.preventDefault();
     try {
-      const token = localStorage.getItem('token');
-      const response = await fetch(`${API_URL}/wp/v2/posts`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${token}`,
-        },
-        body: JSON.stringify({
-          title: title,
-          content: content,
-          status: 'publish',
-        }),
-      });
+      const response = await fetch(
+        'https://pirabu.com/?rest_route=/simple-jwt-login/v1/autologin&JWT=JWT',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            username,
+            password,
+          }),
+        }
+      );
+
       const data = await response.json();
-      if (!response.ok) {
-        setError(data.message);
-        return;
+
+      if (response.ok) {
+        if (data.token) {
+          // Save token to a cookie
+          Cookies.set('token', data.token);
+
+          // Redirect or perform any action after successful login
+        } else {
+          // Handle login failure
+        }
+      } else {
+        // Handle login failure
       }
-      setTitle('');
-      setContent('');
     } catch (error) {
-      console.error(error);
-      setError('An error occurred while creating the post. Please try again later.');
+      // Handle error
     }
-  };
-
-  const fetchUserDetails = async (token) => {
-    try {
-      const response = await fetch(`${API_URL}/wp/v2/users/me`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      const data = await response.json();
-      setUsername(data.name);
-      setEmail(data.email);
-    } catch (error) {
-      console.error(error);
-      setError('An error occurred while fetching user details.');
-    }
-  };
-
-  const handleLogin = async (event) => {
-    event.preventDefault();
-    try {
-      const response = await fetch(`${API_URL}/jwt-auth/v1/token`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          username,
-          password,
-        }),
-      });
-      const data = await response.json();
-      if (!response.ok) {
-        setError(data.message);
-        return;
-      }
-      localStorage.setItem('token', data.token);
-      setIsLoggedIn(true);
-      setError(null);
-      fetchUserDetails(data.token);
-    } catch (error) {
-      console.error(error);
-      setError('An error occurred while logging in. Please try again later.');
-    }
-  };
-
-  const handleLogout = () => {
-    localStorage.removeItem('token');
-    setIsLoggedIn(false);
   };
 
   return (
-    
-    <>
-  {isLoggedIn ? (
-    <>
-      <p>Hello {username}, your email id is {email}!</p>
-      <form onSubmit={handleCreatePost}>
-        <div>
-          <label htmlFor="title">Title</label>
-          <input
-            type="text"
-            id="title"
-            value={title}
-            onChange={(event) => setTitle(event.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="content">Content</label>
-          <textarea
-            id="content"
-            value={content}
-            onChange={(event) => setContent(event.target.value)}
-          />
-        </div>
-        <button type="submit">Create Post</button>
-      </form>
-    </>
-  ) : (
-    <form onSubmit={handleLogin}>
-      <div>
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          id="username"
-          value={username}
-          onChange={(event) => setUsername(event.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-        />
-      </div>
-      <button type="submit">Log in</button>
-      {error && <p>{error}</p>}
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Login</button>
     </form>
-  )}
-  {isLoggedIn && (
-    <button onClick={handleLogout}>Log out</button>
-  )}
-</>
-
   );
-};
-
-export default Login;
+}
