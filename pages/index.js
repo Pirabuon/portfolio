b648ed18ac04e683fa33f4a11ddba89771580027
Pirabuon <40371722+Pirabuon@users.noreply.github.com>
2023-08-17 01:28:59 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -51,7 +51,7 @@ export default function Blog(props) {
 
 export async function getStaticProps() {
   const scienceResponse = await fetch(
-    "https://pirabu.com/wp-json/wp/v2/posts?_embed=true&per_page=4&filter[post_type]=blog"
+    "https://pirabu.com/wp-json/wp/v2/blog?_embed=true&per_page=4"
   );
   const scienceData = await scienceResponse.json();
 
