ca4adb0f2f4e9341a43c1f1e50502ada3176f8a1
Pirabuon <40371722+Pirabuon@users.noreply.github.com>
2023-08-17 02:09:28 +0200

    Update Login.js

--- components/Login.js
+++ components/Login.js
@@ -4,6 +4,8 @@ import Cookies from 'js-cookie';
 export default function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [loginFailed, setLoginFailed] = useState(false); // New state to track login failure
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -27,15 +29,14 @@ export default function Login() {
 
       if (response.ok) {
         if (data.token) {
-          // Save token to a cookie
           Cookies.set('token', data.token);
-
-          // Redirect or perform any action after successful login
+          setLoggedIn(true);
+          setLoginFailed(false); // Reset login failure status
         } else {
-          // Handle login failure
+          setLoginFailed(true); // Set login failure status
         }
       } else {
-        // Handle login failure
+        setLoginFailed(true); // Set login failure status
       }
     } catch (error) {
       // Handle error
@@ -43,20 +44,27 @@ export default function Login() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button type="submit">Login</button>
-    </form>
+    <div>
+      {loggedIn ? (
+        <p>Hello, {username}!</p>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          {loginFailed && <p>Login failed. Please try again.</p>} {/* Show failed message */}
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit">Login</button>
+        </form>
+      )}
+    </div>
   );
 }
